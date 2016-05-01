594529352db02250bc082fad6a3803987648ebd9
linjunjie <linjunjie@raycloud.com>
2016-05-01 21:34:19 +0800

    提交到暂存区

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -5,6 +5,7 @@ public class Controller {
     public static void main(String[] args){
 
         System.out.println("空");
+   这是一个小改动，要提交到暂存区
 
     }
 
---
+++ src/main/java/sample/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by linjunjie(490456661@qq.com) on 2016/5/1.
+ */
+package sample;
